082e3cb631596be7b48752d34a6063cbdc6c973b
imex18 <imex18@outlook.com>
2022-07-07 16:13:46 +0100

    fix (network call) fixed SocketTimeoutException crash

--- app/src/main/java/com/example/techtask/data/RetrofitInstance.kt
+++ app/src/main/java/com/example/techtask/data/RetrofitInstance.kt
@@ -1,6 +1,8 @@
 package com.example.techtask.data
 
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -9,10 +11,16 @@ const val BASE_URL = "https://jsonplaceholder.typicode.com"
 @Singleton
 object RetrofitInstance {
 
+    private val okHttpClient  = OkHttpClient.Builder()
+        .readTimeout(60, TimeUnit.SECONDS)
+        .connectTimeout(60, TimeUnit.SECONDS)
+        .build()
+
     val api: AlbumsApi by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
             .build()
             .create(AlbumsApi::class.java)
     }
